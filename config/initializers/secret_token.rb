86d5c50b189f236e28aef662c1b7d40d34cd923b
Hugo Campos <endlos@hotmail.com>
2012-02-09 17:55:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Choco::Application.config.secret_token = '9afb71726d1d3b3af413bbf4ac9fdcfe7c6b5916df82f52cd27976d8eacc97060bcff899a3ee9986a75582440ad028caac2313d5defa2e20ce27f91adfb6c1a0'
